dd3cf206a37abefdcbd6050e1065965a2d38b336
sisafroo-gif <sisafroo@gmail.com>
2026-01-19 12:32:12 +0300

    added feature:  reserve seats

--- busSeat.cpp
+++ busSeat.cpp
@@ -78,3 +78,56 @@ void showSeats(string seats[], int available_seats) {
     }
     if (!anyReserved) cout << "No seats are reserved yet.\n";
 }
+
+// Reserve seats
+void reserveSeat(string seats[], int &available_seats) {
+    int n;
+    cout << "How many seats do you want to reserve?: ";
+    cin >> n;
+    if (cin.fail()) {
+        cin.clear(); // clear error state
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // discard invalid input
+        cout << "\aInvalid input! Please enter a valid number.\n";
+        return;
+    }
+
+    if (n > available_seats) {
+        cout << "We are sorry but we don't have that many seats available.\n";
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        int seatNum;
+        string name;
+
+        cout << "Enter the seat number you want to reserve (1-50): ";
+        cin >> seatNum;
+        if (cin.fail()) {
+            cin.clear(); // clear error state
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // discard invalid input
+            cout << "\aInvalid input! Please enter a valid seat number.\n";
+            i--; // retry this iteration
+            continue;
+        }
+
+        if (seatNum < 1 || seatNum > TOTAL_SEATS) {
+            cout << "\aInvalid seat number! Try again.\n";
+            i--; // retry this iteration
+            continue;
+        }
+        if (!seats[seatNum - 1].empty()) {
+            cout << "Sorry, this seat is already reserved! Try again.\n";
+            i--; // retry this iteration
+            continue;
+        }
+
+        cin.ignore(); // clear newline
+        cout << "Enter passenger name for seat " << seatNum << ": ";
+        getline(cin, name);
+
+        seats[seatNum - 1] = name;
+        available_seats--;
+        cout << "Seat " << seatNum << " reserved under " << name << ".\n";
+        cout << "Seats left: " << available_seats << "\n\n";
+    }
+}
